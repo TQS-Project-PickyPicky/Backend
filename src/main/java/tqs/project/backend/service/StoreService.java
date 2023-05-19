3671675706cbd34ceaf2c8be77bc370df2757d08
João Fonseca <joao.fonseca@ua.pt>
2023-05-20 00:15:40 +0100

    TP-40 add StoreService tests

--- src/main/java/tqs/project/backend/service/StoreService.java
+++ src/main/java/tqs/project/backend/service/StoreService.java
@@ -1,7 +1,40 @@
 package tqs.project.backend.service;
 
 import org.springframework.stereotype.Service;
+import tqs.project.backend.data.parcel.ParcelRepository;
+import tqs.project.backend.data.store.Store;
+import tqs.project.backend.data.store.StoreRepository;
+
+import java.util.List;
 
 @Service
 public class StoreService {
+
+    private final StoreRepository storeRepository;
+    private final ParcelRepository parcelRepository;
+
+    public StoreService(StoreRepository storeRepository, ParcelRepository parcelRepository) {
+        this.storeRepository = storeRepository;
+        this.parcelRepository = parcelRepository;
+    }
+
+    public Store getStore(Integer id) {
+        return null;
+    }
+
+    public List<Store> getAllStores() {
+        return null;
+    }
+
+    public Store createStore(String name) {
+        return null;
+    }
+
+    public Store updateStore(Integer id, Store store) {
+        return null;
+    }
+
+    public Store deleteStore(Integer id) {
+        return null;
+    }
 }
