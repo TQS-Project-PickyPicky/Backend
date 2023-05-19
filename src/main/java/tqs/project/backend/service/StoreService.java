b474a9b1af41e495abf1a93ccd49c78ed822ab1f
João Fonseca <joao.fonseca@ua.pt>
2023-05-20 00:32:45 +0100

    TP-40 implement StoreService

--- src/main/java/tqs/project/backend/service/StoreService.java
+++ src/main/java/tqs/project/backend/service/StoreService.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Service;
 import tqs.project.backend.data.parcel.ParcelRepository;
 import tqs.project.backend.data.store.Store;
 import tqs.project.backend.data.store.StoreRepository;
+import tqs.project.backend.exception.StoreNotFoundException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,22 +21,31 @@ public class StoreService {
     }
 
     public Store getStore(Integer id) {
-        return null;
+        return storeRepository.findById(id).orElseThrow(() -> new StoreNotFoundException(id));
     }
 
     public List<Store> getAllStores() {
-        return null;
+        return storeRepository.findAll();
     }
 
     public Store createStore(String name) {
-        return null;
+        Store store = new Store();
+        store.setName(name);
+        store.setParcels(new ArrayList<>());
+        return storeRepository.save(store);
     }
 
     public Store updateStore(Integer id, Store store) {
-        return null;
+        Store oldStore = storeRepository.findById(id).orElseThrow(() -> new StoreNotFoundException(id));
+        // Update the store
+        oldStore.setName(store.getName());
+        oldStore.setParcels(store.getParcels());
+        return storeRepository.save(oldStore);
     }
 
     public Store deleteStore(Integer id) {
-        return null;
+        Store store = storeRepository.findById(id).orElseThrow(() -> new StoreNotFoundException(id));
+        storeRepository.delete(store);
+        return store;
     }
 }
