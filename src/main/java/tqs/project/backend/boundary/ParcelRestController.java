24a86627d94c1aa86857895271455679f636364b
João Fonseca <joao.fonseca@ua.pt>
2023-05-20 17:52:26 +0100

    TP-42 add ParcelRestController tests

--- src/main/java/tqs/project/backend/boundary/ParcelRestController.java
+++ src/main/java/tqs/project/backend/boundary/ParcelRestController.java
@@ -1,7 +1,44 @@
 package tqs.project.backend.boundary;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tqs.project.backend.data.parcel.ParcelCreateDto;
+import tqs.project.backend.data.parcel.ParcelDto;
+import tqs.project.backend.data.parcel.ParcelUpdateDto;
+import tqs.project.backend.service.ParcelService;
 
 @RestController
+@RequestMapping("/api")
 public class ParcelRestController {
+
+    private final ParcelService parcelService;
+
+    public ParcelRestController(ParcelService parcelService) {
+        this.parcelService = parcelService;
+    }
+
+    @GetMapping("/parcels/{id}")
+    public ResponseEntity<ParcelDto> getParcel(@PathVariable("id") Integer parcelId) {
+        return null;
+    }
+
+    @GetMapping("/parcels")
+    public ResponseEntity<ParcelDto> getAllParcels() {
+        return null;
+    }
+
+    @PostMapping("/parcels")
+    public ResponseEntity<ParcelDto> createParcel(@RequestBody ParcelCreateDto parcelCreateDto) {
+        return null;
+    }
+
+    @PutMapping("/parcels/{id}")
+    public ResponseEntity<ParcelDto> updateParcel(@PathVariable("id") Integer parcelId, @RequestBody ParcelUpdateDto parcelUpdateDto, @RequestParam(required = false) Integer token) {
+        return null;
+    }
+
+    @DeleteMapping("/parcels/{id}")
+    public ResponseEntity<ParcelDto> deleteParcel(@PathVariable("id") Integer parcelId) {
+        return null;
+    }
 }
