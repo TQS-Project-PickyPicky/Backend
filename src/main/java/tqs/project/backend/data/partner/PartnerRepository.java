29237d58f4c5771c80050e18bbc48d9783ca1ba0
bruna <brunams21@ua.pt>
2023-06-01 19:44:52 +0100

    merge conflits fixed

--- src/main/java/tqs/project/backend/data/partner/PartnerRepository.java
+++ src/main/java/tqs/project/backend/data/partner/PartnerRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PartnerRepository extends JpaRepository<Partner, Integer> {
 
     Partner findByUsernameAndPassword(String username, String password);
-<<<<<<< HEAD
 
     Partner findByCollectionPointId(Integer id);
-=======
->>>>>>> d19509d792dc30898f61775b7a08ead04eb47b97
 }
